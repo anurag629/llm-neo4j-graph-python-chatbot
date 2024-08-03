f765d3009a8b355d8ddcdfe0fe1ee081313a0e3f
anurag629 <akvermaav629@gmail.com>
2024-08-04 02:11:24 +0530

    first time

---
+++ bot.py
@@ -0,0 +1,40 @@
+import streamlit as st
+from utils import write_message
+
+# Page Config
+st.set_page_config("Ebert", page_icon=":movie_camera:")
+
+# Set up Session State
+if "messages" not in st.session_state:
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Hi, I'm the Neo4j Graph LLM Chatbot!  How can I help you?"},
+    ]
+
+# Submit handler
+def handle_submit(message):
+    """
+    Submit handler:
+
+    You will modify this method to talk with an LLM and provide
+    context using data from Neo4j.
+    """
+
+    # Handle the response
+    with st.spinner('Thinking...'):
+        # # TODO: Replace this with a call to your LLM
+        from time import sleep
+        sleep(1)
+        write_message('assistant', message)
+
+
+# Display messages in Session State
+for message in st.session_state.messages:
+    write_message(message['role'], message['content'], save=False)
+
+# Handle any user input
+if question := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    write_message('user', question)
+
+    # Generate a response
+    handle_submit(question)
